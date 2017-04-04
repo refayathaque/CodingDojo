efe5f74f15ec9e4586b98c60b24509472fa75e95
Refayat Haque <refayathaque@gmail.com>
2017-04-03 23:17:58 -0400

    Updates

--- preworknotes.js
+++ preworknotes.js
@@ -1,11 +1,18 @@
 function greetSomeone(person){
   var person1 = person.toLowerCase();
-  if (person1 == "refayat"){
-    console.log("Hey there Refayat!");
-}
-else {
-  console.log("Hey there other guy!")
-}
+  if (person1 == "refayat"){console.log("Hey there Refayat!");}
+  else {console.log("Hey there other guy!");}
 }
 
 greetSomeone("Refayat")
+greetSomeone("refayat")
+greetSomeone("George")
+
+function printMyName(name){
+  console.log(name);
+}
+
+printMyName("John")
+
+var othername = printMyName("Harry")
+console.log(othername); // Outputs 'undefined', not really sure why...
