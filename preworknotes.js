5979de10c1c8e179cd422473388ff3ce398ef043
Refayat Haque <refayathaque@gmail.com>
2017-04-04 23:11:01 -0400

    added an example

--- preworknotes.js
+++ preworknotes.js
@@ -22,3 +22,22 @@ printMyName("John")
 
 var othername = printMyName("Harry")
 console.log(othername); // Outputs 'undefined', not really sure why...
+
+// The most difficult challenge yet in the Algorithms App
+
+var array = [5, 3, 4, 1]
+
+for(var x=0; x<array.length; x++)
+{
+  for(var y=array.length-1; y>=x; y--)
+  {
+    if(array[x] > array[y])
+    {
+      var temp = array[y];
+      array[y] = array[x];
+      array[x] = temp;
+    }
+  }
+}
+
+console.log(array) // Outputs [1, 3, 4, 5], no idea how...
