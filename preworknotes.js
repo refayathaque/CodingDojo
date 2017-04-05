1840c1a638fc0e5e6e897991643299bd6a9eaeed
Refayat Haque <refayathaque@gmail.com>
2017-04-04 23:07:21 -0400

    Tweaks

--- preworknotes.js
+++ preworknotes.js
@@ -1,7 +1,13 @@
 function greetSomeone(person){
   var person1 = person.toLowerCase();
-  if (person1 == "refayat"){console.log("Hey there Refayat!");}
-  else {console.log("Hey there other guy!");}
+  if (person1 == "refayat")
+  {
+    console.log("Hey there Refayat!");
+  }
+  else
+  {
+    console.log("Hey there other guy!");
+  }
 }
 
 greetSomeone("Refayat")
