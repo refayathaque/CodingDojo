0b02b9002b55b7c31e61ee904e99bed73cc4f604
Refayat Haque <refayathaque@gmail.com>
2017-04-06 22:40:28 -0400

    .

--- test.js
+++ test.js
@@ -1,1 +1,11 @@
-console.log("hello")
+function greaterY(arr, Y) {
+  count = 0;
+  for(var x = 0; x < arr.length; x++) {
+    if(arr[x] > Y) {
+      count++;
+    }
+  }
+  console.log(count);
+}
+
+greaterY([1, 4, 7, 6], 3)
