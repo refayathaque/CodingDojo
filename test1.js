32169935f9ed7d0bcb56a3e34055cd7159126cac
Refayat Haque <refayathaque@gmail.com>
2017-04-18 14:55:04 -0400

    working on CSS

--- test1.js
+++ test1.js
@@ -1,11 +1,9 @@
-function moveValuesLeft(arr) {
-  for(var x = 0; x < arr.length; x++) {
-    arr[x] = arr[x + 1];
-    if(x === arr.length - 1) {
-      arr[x] = 0;
-    }
+function fahToCel(fDg) {
+  var cDg = 0;
+  for(var x = 0; x < fDg.length; x++) {
+    cDg = 5/9 * (fDg[x] - 32);
+    console.log(cDg);
   }
-  console.log(arr);
 }
 
-moveValuesLeft([1, 2, 3, 4, 5]);
+fahToCel([32, 45, 89]);
